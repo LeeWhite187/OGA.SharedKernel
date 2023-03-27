b5d165271041eb9f06b81f53f19455e6fb98c8e3
LeeWhite187 <LeeWhite187@gmail.com>
2023-03-27 00:52:50 -0400

    Added special void type. This allows generics to be of a type void. Such as web returns that contain no data, just a 200.
    Added Serialization helper class that better determines the string-name of generic types.
    
    +semver:minor

---
+++ OGA.SharedKernel/OGA.SharedKernel_SP/SpecialTypes/Serialization_Helper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oga.Common.SpecialTypes
+{
+    public class Serialization_Helper
+    {
+        /// <summary>
+        /// Call this method to get a string name type of the given object.
+        /// Set usefullnames to include the assembly with each classname.
+        /// Return type is of the form:
+        /// "string"
+        /// "List<string>"
+        /// "DistributionWrapper<SendCommand>"
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="usefullnames"></param>
+        /// <returns></returns>
+        static public string GetType_forSerialization(object obj, bool usefullnames = false)
+        {
+            return GetType_forSerialization(obj.GetType(), usefullnames);
+        }
+
+        /// <summary>
+        /// Call this method to get a string name type of the given type.
+        /// Set usefullnames to include the assembly with each classname.
+        /// Return type is of the form:
+        /// "string"
+        /// "List<string>"
+        /// "DistributionWrapper<SendCommand>"
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="usefullnames"></param>
+        /// <returns></returns>
+        static public string GetType_forSerialization(Type type, bool usefullnames = false)
+        {
+            StringBuilder retType = new StringBuilder();
+
+            if (type.IsGenericType)
+            {
+                string[] parentType = new string[]{ };
+                if (usefullnames)
+                    parentType = type.FullName != null ? type.FullName.Split('`') : type.Name.Split('`');
+                else
+                    parentType = type.Name.Split('`');
+
+                // We will build the type here.
+                Type[] arguments = type.GetGenericArguments();
+
+                StringBuilder argList = new StringBuilder();
+                
+                foreach (Type t in arguments)
+                {
+                    // Let's make sure we get the argument list.
+                    string arg = GetType_forSerialization(t, usefullnames);
+                    if (argList.Length > 0)
+                    {
+                        argList.AppendFormat(", {0}", arg);
+                    }
+                    else
+                    {
+                        argList.Append(arg);
+                    }
+                }
+
+                if (argList.Length > 0)
+                {
+                    retType.AppendFormat("{0}<{1}>", parentType[0], argList.ToString());
+                }
+            }
+            else
+            {
+                return (usefullnames ? type.ToString() : type.Name);
+            }
+
+            return retType.ToString();
+        }
+    }
+}
---
+++ OGA.SharedKernel/OGA.SharedKernel_SP/SpecialTypes/Void.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace oga.SharedKernel.SpecialTypes
+{
+    /// <summary>
+    /// This struct is a simple implementation of the builtin "void".
+    /// It can be used in a generic, to make a concrete class of type T as void.
+    /// This overcomes the language limitation that c# will not allow a generic to be concreted to type void.
+    /// This struct may have additional purposes, beyond its original intention.
+    /// But, it was originally created to allow a generalized web client method (one that can send and receive any type)
+    ///     to accept nothing as a reply, which allows the generalized method call to accept a simple Ok (200) and no response body.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Size = 0)]
+    public struct Void { }
+}
